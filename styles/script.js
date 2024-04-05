832137362501fdfcf8aa48c806c419018896af6e
TwoStepTim <tjmims33@gmail.com>
2024-04-04 22:37:03 -0400

    Major update

--- styles/script.js
+++ styles/script.js
@@ -7,8 +7,8 @@ const sidebarCheckbox = document.getElementById('sidebar-checkbox'); // Get the 
 function closeNavBar() {
     navbar_moblie.style.opacity = '0';
     navbar_moblie.style.transform = 'translatex(-100%)';
-    hamburger.style.color = 'rgb(255, 255, 255)';
-   // Uncheck the checkbox to close the navigation bar
+    hamburger.style.color = '#82641c';
+    // Uncheck the checkbox to close the navigation bar
 }
 
 hamburger.addEventListener('click', () => {
@@ -17,14 +17,26 @@ hamburger.addEventListener('click', () => {
     } else {
         navbar_moblie.style.opacity = '1';
         navbar_moblie.style.transform = 'translatex(0)';
-        hamburger.style.color = 'rgb(0, 0, 0)';
+        hamburger.style.color = '#82641c';
     }
 }
-
-
-
-
-
-
-
 );
+
+// Toggle dropdown menu when clicking on dropdown button
+document.addEventListener('click', function(event) {
+    var dropdownButton = event.target.closest('.dropdown') ? event.target.closest('.dropdown').querySelector('.dropbtn') : null;
+    var dropdownContent = event.target.closest('.dropdown') ? event.target.closest('.dropdown').querySelector('.dropdown-content') : null;
+    
+    if (dropdownButton && dropdownContent) {
+        if (event.target === dropdownButton) {
+            if (dropdownContent.style.display === 'block') {
+                dropdownContent.style.display = 'none';
+            } else {
+                dropdownContent.style.display = 'block';
+            }
+        } else {
+            dropdownContent.style.display = 'none';
+        }
+    }
+});
+
